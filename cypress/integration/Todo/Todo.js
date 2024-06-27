af1258a057738874ae01300530683a5b88303466
aziz-kck <61433292+aziz-kck@users.noreply.github.com>
2024-06-27 17:06:02 +0100

    Update Todo.js

--- cypress/integration/Todo/Todo.js
+++ cypress/integration/Todo/Todo.js
@@ -1,4 +1,4 @@
-/*describe('Cypress Test on Todo Project', function() {
+describe('Cypress Test on Todo Project', function() {
   it('visits My Todo App', function() {
     cy.visit('/')
   })
@@ -59,72 +59,4 @@
     cy.get('.todo').should('have.length', 2)
   })
 }) 
-*/
-describe('Cypress Test on Todo Project', function() {
-  it('visits My Todo App', function() {
-    cy.visit('/');
-  });
 
-  it('should focus on the input', () => {
-    cy.focused().should('have.class', 'input');
-  });
-
-  it('initially has 3 todos', () => {
-    cy.get('.todo').should('have.length', 3);
-  });
-
-  it('has "React Hooks" at first todo', () => {
-    cy.get('.todo')
-      .first()
-      .children()
-      .first()
-      .should('have.text', 'React Hooks');
-  });
-
-  it('has "Cypress.io" at 3rd todo', () => {
-    cy.get('.todo')
-      .eq(2)
-      .children()
-      .first()
-      .should('have.text', 'Cypress.io');
-  });
-
-  it('adds a new todo', () => {
-    const txt = 'Learn about Cypress';
-    cy.get('.input')
-      .type(txt)
-      .type('{enter}')
-      .get('.todo')
-      .should('have.length', 4)
-      .wait(1000); // Wait for new todo to be added
-  });
-
-  it('checks first todo', () => {
-    cy.get('.btn-check')
-      .first()
-      .click();
-
-    cy.get('.todo')
-      .first()
-      .should('have.css', 'text-decoration')
-      .and('match', /line-through/)
-      .wait(500); // Wait for check action to complete
-  });
-
-  it('removes last 2 todos', () => {
-    cy.get('.btn-close')
-      .last()
-      .click();
-
-    cy.get('.btn-close')
-      .last()
-      .click();
-
-    cy.get('.todo').should('have.length', 2)
-      .wait(1000); // Wait for todos to be removed
-  });
-});
-
-
-
-  
